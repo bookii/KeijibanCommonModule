da0a8a77e244b8281029a8c29c7fbc519afb8441
mizznoff <mizznoff+dev@gmail.com>
2025-12-14 11:30:37 +0900

    Board と Entry の DTO を定義する

---
+++ Sources/DTO/KCMBoardDTO.swift
@@ -0,0 +1,13 @@
+//
+//  KCMBoardDTO.swift
+//  KeijibanCommonModule
+//
+//  Created by mizznoff on 2025/12/14.
+//
+
+import Foundation
+
+public struct KCMBoardDTO: Codable {
+    public let id: UInt64
+    public let name: String
+}
