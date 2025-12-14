42a68c7f360732ec5801c26ce8cf3c42dfe45e36
mizznoff <mizznoff+dev@gmail.com>
2025-12-14 13:43:39 +0900

    format

--- Package.swift
+++ Package.swift
@@ -9,15 +9,14 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "KeijibanCommonModule",
-            targets: ["KeijibanCommonModule"]
+            targets: ["KeijibanCommonModule"],
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "KeijibanCommonModule"
+            name: "KeijibanCommonModule",
         ),
-
-    ]
+    ],
 )
--- Sources/DTO/KCMBoardDTO.swift
+++ Sources/DTO/KCMBoardDTO.swift
@@ -10,7 +10,7 @@ import Foundation
 public struct KCMBoardDTO: Codable {
     public let id: UUID?
     public let name: String
-    
+
     public init(id: UUID?, name: String) {
         self.id = id
         self.name = name
