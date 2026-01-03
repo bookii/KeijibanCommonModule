c10cde66f41eb16d679ecbf693b00c44881b876e
mizznoff <mizznoff+dev@gmail.com>
2026-01-04 00:59:15 +0900

    KCMPostEntriesRequestBody の修正

--- Sources/RequestParams/KCMPostEntriesRequestBody.swift
+++ Sources/RequestParams/KCMPostEntriesRequestBody.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public struct KCMPostEntriesRequestBody: Codable {
-    public let base64EncodedStrings: [String]
+    public let wordImages: [KCMWordImageDTO]
     public let authorName: String
     public let deleteKey: String
 
     private enum CodingKeys: String, CodingKey {
-        case base64EncodedStrings = "base64_encoded_strings"
+        case wordImages = "word_images"
         case authorName = "author_name"
         case deleteKey = "delete_key"
     }
